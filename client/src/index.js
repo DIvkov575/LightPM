a510bfa27782ba8cdf1cc9bc67f15f6d48e9d9f3
Divkov575 <dmitriy@ivkov.net>
2023-06-20 19:13:14 -0400

    added sign up page

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './home/App';
+import Signup from './signup/signup';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Signup />
   </React.StrictMode>
 );
 
--- server/routes/record.js
+++ server/routes/record.js
@@ -1,9 +1,6 @@
 const express = require("express");
-// recordRoutes is an instance of the express router.
 const recordRoutes = express.Router();
-// This will help us connect to the database
 const dbo = require("../db/conn");
- 
 // This helps convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
  
