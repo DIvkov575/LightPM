72ffe6a563b1f505e3027e1292e7618c0dd42a1d
Divkov575 <dmitriy@ivkov.net>
2023-06-25 17:47:20 -0400

    adde calendar

--- client/src/pages/showListing/showListing.tsx
+++ client/src/pages/showListing/showListing.tsx
@@ -1,26 +1,32 @@
 import './showListing.scss'
 import {useParams} from "react-router";
+import React from "react";
+// @ts-ignore
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+interface dataProp {
+    dataKey: string,
+    dataVal: string,
+}
+const ShowData: React.FC<dataProp> = (props) => {
+    return (
+        <div>
+           <h3></h3>
+        </div>
+    )
+
+}
 
 function ShowListing() {
     const {id} = useParams();
+    let listingData = JSON.parse(sessionStorage.getItem("properties") as string)[parseInt(id as string)];
     return (
         <div>
-        <h1>{id}</h1>
-                    {/*
-                    TODO:
-                    - images (big + carousel underneath)
-                    - link to other various platform profiles + status
-                    - reservation calendar
-                    - edit listing section
-                        - description
-                        - tags
-                        - features   
+        <h1>{listingData.name}</h1>
+        <h1>{listingData.location}</h1>
 
-                    
-                    
-                    
-                    
-                    */}
+        <Calendar />
         </div>
 
 
